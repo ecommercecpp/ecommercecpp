786007e5de648cf81ac53280c92b14979526e134
CaioHosken <caiohoskenmachado@gmail.com>
2022-06-09 13:23:46 -0300

    Update atributos.hpp

---
+++ include/produtos/atributos/atributos.hpp
@@ -0,0 +1,14 @@
+#include <map>
+
+class Atributos
+{
+	private:
+		map<string, map<string, string>> ListaAtributos;
+	public:
+		Atributos();
+		map<string, map<string, string>> getListaAtributos();
+		void setListaAtributos();
+		map<string, string> getListaAtributos(string nomeAtributo);
+		void setAtributo(string nomeAtributo);
+		void adicionarAtributo(string nomeAtributo, map<string, string> niveis);
+}
