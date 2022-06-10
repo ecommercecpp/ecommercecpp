18bd5ff5cf458d4eb83df53c390ad4f24b22dc79
Felippe Veloso <60450622+FelippeVelosoMarinho@users.noreply.github.com>
2022-06-10 00:20:43 -0300

    Update da classe definitiva para produtos.

--- include/produtos/produto.hpp
+++ include/produtos/produto.hpp
@@ -4,7 +4,38 @@
 
 #include "../autoload.hpp"
 
+#include <string>
+
+#include "./atributos/atributos.hpp"
+
 class Produto {
+ public:
+
+  Produto(unsigned int id, std::string nome, std::string descricao, double qtd);
+  Produto();
+
+  unsigned int getId();
+
+  std::string getNome();
+
+  std::string getDescricao();
+
+  double getQtd();
+
+  void setId(unsigned int id);
+
+  void setNome(std::string nome);
+
+  void setDescricao(std::string descricao);
+
+  void setQtd(double qtd);
+
+ private:
+    unsigned int _id;
+    std::string _nome;
+    std::string _descricao;
+    double _qtd;
+    Atributos _atributos;
 };
 
 #endif
