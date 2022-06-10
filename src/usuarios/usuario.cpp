2900d4986d369c50aa297f07b325851f586d8ea5
aquila1004 <97201817+aquila1004@users.noreply.github.com>
2022-06-10 16:13:45 -0300

    feat-implementação-classe-usuário

---
+++ src/usuarios/usuario.cpp
@@ -0,0 +1,88 @@
+#include "../../../include/autoload.hpp"
+
+/**
+ * @brief Construtor para a Classe Usuário
+ * 
+ */
+Usuario::Usuario(std::string nome, int cpf, std::string email, Endereco endereco, std::string senha){
+    this->nome = nome;
+    this->cpf = cpf;
+    this->email = email;
+    this->endereco = endereco;
+    this->senha = senha;
+}
+/**
+ * @brief seta o nome
+ * 
+ */
+void Usuario::setNome(std::string nome){
+    this->nome = nome;
+}
+/**
+ * @brief seta o cpf do usuario
+ * 
+ */
+void Usuario::setCpf(int cpf){
+    this->cpf = cpf;
+}
+/**
+ * @brief seta o email do usuario
+ * 
+ */
+void Usuario::setEmail(std::string email){
+    this->email = email;
+}
+/**
+ * @brief seta o endereço do usuario
+ * 
+ */
+void Usuario::setEndereco(Endereco endereco){
+    this->endereco = endereco;
+}
+/**
+ * @brief seta a senha do usuario
+ * 
+ */
+void Usuario::setSenha(std::string senha){
+    this->senha = senha;
+}
+/**
+ * @brief Retorna o nome do usuario
+ * 
+ * @return std::string 
+ */
+std::string Usuario::getNome(){
+    return this->nome;
+}
+/**
+ * @brief Retorna o cpf do usuario
+ * 
+ * @return int 
+ */
+int Usuario::getCpf(){
+    return this->cpf;
+}
+/**
+ * @brief Retorna o email do usuario
+ * 
+ * @return std::string 
+ */
+std::string Usuario::getEmail(){
+    return this->email;
+}
+/**
+ * @brief Retorna o endereço do usuario
+ * 
+ * @return std::string 
+ */
+Endereco Usuario::getEndereco(){
+    return this->endereco;
+}
+/**
+ * @brief Retorna a senha do usuario
+ * 
+ * @return std::string 
+ */
+std::string Usuario::getSenha(){
+    return this->senha;
+}
