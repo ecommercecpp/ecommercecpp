f16e7850fd0bda1eed69bcbadafd36645b0d455c
EduScf <eduardoscf1210@gmail.com>
2022-06-10 22:54:20 -0300

    chore: Criação da documentação doxygen no estoqueBase.cpp
    Criação da documentação doxygen no estoqueBase.cpp

--- src/loja/estoque/base/estoqueBase.cpp
+++ src/loja/estoque/base/estoqueBase.cpp
@@ -1,18 +1,42 @@
 #include "../../../../include/autoload.hpp"
 
+/**
+ * @brief Adiciona um produto ao estoque.
+ *
+ * @param  Produto produto
+ */
 void EstoqueBase::adicionar(Produto produto){
     estoque.insert(std::pair<int, Produto>(produto.getId(), produto));
 }
 
+/**
+ * @brief Remove um produto do estoque.
+ *
+ * @param  int id
+ */
 void EstoqueBase::remover(int id){
     estoque.erase(id);
 }
 
+/**
+ * @brief Busca um produto no estoque.
+ *
+ * @param  int id
+ *
+ * @return Produto*  
+ */
 Produto* EstoqueBase::buscar(int id){
     return &estoque.at(id);
 }
 
+/** @brief Retorna o estoque.
+ *
+ * @return  std::map<int, Produto>*
+ */
+
 std::map<int, Produto>* EstoqueBase::getEstoque(){
     return &estoque;
 }
+
+
 
