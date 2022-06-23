b5cc35199ae7ad244eabbc9e7686b587e9685d5a
Felippe Veloso <60450622+FelippeVelosoMarinho@users.noreply.github.com>
2022-06-23 11:23:05 -0300

    Update atributos.cpp

--- src/produtos/atributos/atributos.cpp
+++ src/produtos/atributos/atributos.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <map>
 
-#include "../../../include/produtos/atributos/atributos.hpp"
+#include "../../../include/autoload.hpp"
 
 
 std::map<std::string, std::map<std::string, std::string>> Atributos::getListaAtributos(){
@@ -11,7 +11,7 @@ std::map<std::string, std::map<std::string, std::string>> Atributos::getListaAtr
 
 void Atributos::setListaAtributos(){// Perguntar o que é pra ser feito aqui
     this->ListaAtributos = getListaAtributos();
-}
+}// Nesse set, eu fiz o map da ListaAtributos receber o que foi passado no getLista porém não tenho certeza se está correto.
 
 std::map<std::string, std::string> Atributos::getListaAtributos(std::string nomeAtributo){
     //retornar anome do atributo na lista
@@ -19,13 +19,14 @@ std::map<std::string, std::string> Atributos::getListaAtributos(std::string nome
 }// ok
 
 void Atributos::setAtributo(std::string nomeAtributo){
-    setAtributo = nomeAtributo;
-}// ok
+    //setAtributo = nomeAtributo;
+}// Nesse set, como eu não uma variável de nomeAtributo iniciada no construtor, o que eu coloco pra receber
+// o nomeAtributo do parâmetro?
 
 void Atributos::adicionarAtributo(std::string nomeAtributo, std::map<std::string, std::string> niveis){
     //adicionar atributos de nome,niveis no mapa de listaAtributos
     std::map<std::string, std::map<std::string, std::string>>::iterator it;
-    for (auto it = this->ListaAtributos.begin(); it != this->ListaAtributos.end(); ++it) {
-        this->ListaAtributos.insert(std::pair<std::string, std::map<std::string, std::string>>(getListaAtributos(nomeAtributo), niveis));
-    }
+    for (auto it = istaAtributos.begin(); it != ListaAtributos.end(); ++it) {
+        //ListaAtributos.insert({std::pair<std::string, std::map<std::string, std::string>>(getListaAtributos(nomeAtributo), niveis)});
+    }//Não sei exatamente como inserir o atributo no map ;-;
 }
