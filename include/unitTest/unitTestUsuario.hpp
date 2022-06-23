7b60feba177451cfa2389dd99a0f8f6d2ef4ff9e
aquila1004 <97201817+aquila1004@users.noreply.github.com>
2022-06-22 21:08:58 -0300

    feat: teste usuario

---
+++ include/unitTest/unitTestUsuario.hpp
@@ -0,0 +1,12 @@
+#ifndef _UNIT_TEST_USUARIO_HPP_
+#define _UNIT_TEST_USUARIO_HPP_
+
+#include "../autoload.hpp"
+
+class UnitTestUsuario : public UnitTest
+{
+    public:
+        void run() override;
+};
+
+#endif
---
+++ src/unitTest/unitTestUsuario.cpp
@@ -0,0 +1,36 @@
+#include "../../include/autoload.hpp"
+
+void UnitTestUsuario::run()
+{
+    try{
+        // criar um vetor de 100 usuários aleatórios
+        std::vector<Usuario*> usuarios;
+        for(int i = 0; i < 100; i++){
+            Usuario *usuario = new Usuario(
+                "Nome " + std::to_string(i),
+                "Sobrenome " + std::to_string(i),
+                "Email " + std::to_string(i),
+                "Senha " + std::to_string(i)
+            );
+            usuarios.push_back(usuario);
+        }
+        //remove os usuários
+        for(int i = 0; i < 100; i++){
+            delete usuarios[i];
+        }
+        //remove o vetor
+        usuarios.clear();
+        // thrown an error if the vector is not empty
+        if(usuarios.size() != 0){
+            throw std::runtime_error("The vector is not empty");
+        }
+        // pass the test
+        pass();
+        catch(std::exception &e){
+            // fail the test
+            fail(std::string(e.what()));
+        }
+    }
+    
+    
+}
