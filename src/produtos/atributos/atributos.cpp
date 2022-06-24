626358207704ae64b3861ad86d67f847c4f7f784
Felippe Veloso <felippevm@ufmg.br>
2022-06-24 15:17:38 -0300

    Update atributos.cpp

--- src/produtos/atributos/atributos.cpp
+++ src/produtos/atributos/atributos.cpp
@@ -9,19 +9,19 @@ std::map<std::string, std::map<std::string, std::string>> Atributos::getListaAtr
     return this->ListaAtributos;
 }// ok 
 
-void Atributos::setListaAtributos(){// Perguntar o que é pra ser feito aqui
+/*void Atributos::setListaAtributos(){// Perguntar o que é pra ser feito aqui
     this->ListaAtributos = getListaAtributos();
-}// Nesse set, eu fiz o map da ListaAtributos receber o que foi passado no getLista porém não tenho certeza se está correto.
+}*/// Nesse set, eu fiz o map da ListaAtributos receber o que foi passado no getLista porém não tenho certeza se está correto.
 
 std::map<std::string, std::string> Atributos::getListaAtributos(std::string nomeAtributo){
     //retornar anome do atributo na lista
     return nomeAtributo;
 }// ok
 
-void Atributos::setAtributo(std::string nomeAtributo){
+/*void Atributos::setAtributo(std::string nomeAtributo){
     //setAtributo = nomeAtributo;
 }// Nesse set, como eu não uma variável de nomeAtributo iniciada no construtor, o que eu coloco pra receber
-// o nomeAtributo do parâmetro?
+// o nomeAtributo do parâmetro?*/
 
 void Atributos::adicionarAtributo(std::string nomeAtributo, std::map<std::string, std::string> niveis){
     //adicionar atributos de nome,niveis no mapa de listaAtributos
