6c22bfeb55f873e64fd496031ed8f9bab3a7cd62
Felippe Veloso <felippevm@ufmg.br>
2022-06-24 16:17:50 -0300

    Create unitTestProdutos.hpp

---
+++ include/unitTest/unitTestProdutos.hpp
@@ -0,0 +1,19 @@
+#ifndef _UNIT_TEST_PRODUTOS_HPP_
+#define _UNIT_TEST_PRODUTOS_HPP_
+
+#include "../autoload.hpp"
+
+class UnitTest
+{
+	protected:
+		bool passed = false;
+		std::string error;
+	public:
+		void pass();
+		void fail(std::string error);
+		virtual void run() = 0;
+		~UnitTest();
+		void throwError(std::string error);
+};
+
+#endif
