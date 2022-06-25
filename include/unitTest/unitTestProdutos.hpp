4972824623c2686730c5439ad9021590287096a8
Felippe Veloso <60450622+FelippeVelosoMarinho@users.noreply.github.com>
2022-06-24 23:04:54 -0300

    Update unitTesteProdutos.hpp

--- include/unitTest/unitTestProdutos.hpp
+++ include/unitTest/unitTestProdutos.hpp
@@ -3,17 +3,36 @@
 
 #include "../autoload.hpp"
 
-class UnitTest
+//testar sets e gets - getId e setId - 
+
+void UnitTestNome::run()//nao pode retornar um caracter vazio e nao pode ser igual hah algum nome já registrado
 {
-	protected:
-		bool passed = false;
-		std::string error;
-	public:
-		void pass();
-		void fail(std::string error);
-		virtual void run() = 0;
-		~UnitTest();
-		void throwError(std::string error);
-};
+	try
+	{
+		bool teste = false;
+   		// seu código normal
+		// ...
+		// aqui, caso um valor não for true, lança um erro:
+		if(Produto::getNome() == ""){
+			teste = false;
+		}
+		//como consultar nomes que jah existem no banco?
+
+		if(teste == true)
+		{
+			throw std::runtime_error("a variável 'teste' não é true");
+		}
+
+		// caso passar em todos os testes, use pass(); para passar o teste
+		pass();
+	}
+	catch ( std::exception &e )
+	{
+		std::string aux;
+		cin>>aux;
+		Produto::setNome(aux);
+		fail(std::string(e.what()));
+	}
+}
 
 #endif
