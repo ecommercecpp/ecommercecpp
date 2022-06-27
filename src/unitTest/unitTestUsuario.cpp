082a4b6dfd5ab2c8da10ef719dd9ca216961cc0b
Leonardo Oliveira <leooli.teixeira@gmail.com>
2022-06-27 14:47:47 -0300

    chore: :ambulance: adicionado mais testes classe usuarios

--- src/unitTest/unitTestUsuario.cpp
+++ src/unitTest/unitTestUsuario.cpp
@@ -22,6 +22,43 @@ void UnitTestUsuario::run()
             );
             usuarios.push_back(usuario);
         }
+
+        for(int i = 0; i < 100; i++)
+        {
+            if(usuarios[i]->getNome() != "Nome " + std::to_string(i))
+                throw std::runtime_error("Erro ao testar o nome do usuário " + std::to_string(i));
+            if(usuarios[i]->getCpf() != i)
+                throw std::runtime_error("Erro ao testar o cpf do usuário " + std::to_string(i));
+            if(usuarios[i]->getEmail() != "Email " + std::to_string(i))
+                throw std::runtime_error("Erro ao testar o email do usuário " + std::to_string(i));
+            if(usuarios[i]->getEndereco().getRua() != "Rua " + std::to_string(i))
+                throw std::runtime_error("Erro ao testar o rua do usuário " + std::to_string(i));
+            if(usuarios[i]->getEndereco().getNumero() != i)
+                throw std::runtime_error("Erro ao testar o numero do usuário " + std::to_string(i));
+            if(usuarios[i]->getEndereco().getComplemento() != "Complemento " + std::to_string(i))
+                throw std::runtime_error("Erro ao testar o complemento do usuário " + std::to_string(i));
+            if(usuarios[i]->getEndereco().getBairro() != "Bairro " + std::to_string(i))
+                throw std::runtime_error("Erro ao testar o bairro do usuário " + std::to_string(i));
+            if(usuarios[i]->getEndereco().getCidade() != "Cidade " + std::to_string(i))
+                throw std::runtime_error("Erro ao testar a cidade do usuário " + std::to_string(i));
+            if(usuarios[i]->getEndereco().getEstado() != "Estado " + std::to_string(i))
+                throw std::runtime_error("Erro ao testar o estado do usuário " + std::to_string(i));
+            if(usuarios[i]->getEndereco().getCep() != i)
+                throw std::runtime_error("Erro ao testar o cep do usuário " + std::to_string(i));
+            if(usuarios[i]->getSenha() != "Senha " + std::to_string(i))
+                throw std::runtime_error("Erro ao testar a senha do usuário " + std::to_string(i));
+        }
+
+        // testa todos os sets
+        for(int i = 0; i < 100; i++)
+        {
+            usuarios[i]->setNome("Nome " + std::to_string(i));
+            usuarios[i]->setCpf(i);
+            usuarios[i]->setEmail("Email " + std::to_string(i));
+            usuarios[i]->setEndereco(Endereco("Rua " + std::to_string(i), i, "Complemento " + std::to_string(i), i, "Bairro " + std::to_string(i), "Cidade " + std::to_string(i), "Estado " + std::to_string(i)));
+            usuarios[i]->setSenha("Senha " + std::to_string(i));
+        }
+
         //remove os usuários
         for(int i = 0; i < 100; i++){
             delete usuarios[i];
