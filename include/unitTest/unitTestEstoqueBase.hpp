eb6d60ca2d13ea8399cc63be6f11b783a77c2edd
CaioHosken <caiohoskenmachado@gmail.com>
2022-06-28 17:36:22 -0300

    feat: criação do hpp e cpp do unitTestEstoqueBase

---
+++ include/unitTest/unitTestEstoqueBase.hpp
@@ -0,0 +1,12 @@
+#ifndef _UNIT_TEST_ESTOQUE_BASE_HPP_
+#define _UNIT_TEST_ESTOQUE_BASE_HPP_
+
+#include "../autoload.hpp"
+
+class UnitTestEndereco : public UnitTest
+{
+	public:
+		void run() override;
+};
+
+#endif
---
+++ src/unitTest/unitTestEstoqueBase.cpp
@@ -0,0 +1,50 @@
+#include "../../include/autoload.hpp"
+
+/**
+ * @brief Teste unitário para a classe EstoqueBase
+ * 
+ */
+void UnitTestEstoqueBase::run()
+{
+	try {
+
+		// cria um estoque
+		EstoqueBase* estoqueX = new EstoqueBase();	
+		
+
+		// adiciona 50 produtos ao estoque
+		
+		for(int i = 1; i < 51; i++)
+		{
+			estoqueX->EstoqueBase::adicionar(Produto(i, "nomeProduto", "descricao qualquer", i));
+		}
+
+		// busca todos produtos
+		for(int i = 1; i < 51; i++)
+		{
+			estoqueX->EstoqueBase::buscar(i);
+		}
+		
+
+		// retorna o estoque
+		estoqueX.EstoqueBase::getEstoque();
+
+		// remove todos produtos
+		for(int i = 1; i < 51; i++)
+		{
+			estoqueX.EstoqueBase::remover(i);
+		}
+		
+
+		//deleta o  estoque
+		delete estoqueX;
+
+		// pass the test
+		pass();
+	}
+	catch ( std::exception &e )
+	{
+		// fail the test
+		fail(std::string(e.what()));
+	}
+}
