a06cf94a7a0f54080f5649463578180118fca6ba
CaioHosken <caiohoskenmachado@gmail.com>
2022-06-28 22:38:01 -0300

    feat: criação método construtor da classe EstoqueBase

--- src/loja/estoque/base/estoqueBase.cpp
+++ src/loja/estoque/base/estoqueBase.cpp
@@ -1,6 +1,16 @@
 #include "../../../../include/autoload.hpp"
 
 /**
+ * @brief Construct a new EstoqueBase:: EstoqueBase object
+ * 
+ */
+
+EstoqueBase::EstoqueBase()
+{
+
+}
+
+/**
  * @brief Adiciona um produto ao estoque.
  *
  * @param  Produto produto
