9d21d351610631b794e83676ce6ecae75285dc9e
Leonardo Oliveira <leooli.teixeira@gmail.com>
2022-07-02 23:14:34 -0300

    test

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,12 @@
 // int main com args
 int main(int argc, char *argv[])
 {
+	if (__cplusplus == 201703L) std::cout << "C++17\n";
+    else if (__cplusplus == 201402L) std::cout << "C++14\n";
+    else if (__cplusplus == 201103L) std::cout << "C++11\n";
+    else if (__cplusplus == 199711L) std::cout << "C++98\n";
+    else std::cout << "pre-standard C++\n";
+	
 	CLI cli(argc, argv);
 	return 0;
 }
