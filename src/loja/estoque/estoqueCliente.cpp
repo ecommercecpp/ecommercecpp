82d47b60918235ead4f7af5512722fabfc7612b6
Felippe Veloso <felippevm@ufmg.br>
2022-07-10 01:02:44 -0300

    feat: Adicionando função listarEstoque

--- src/loja/estoque/estoqueCliente.cpp
+++ src/loja/estoque/estoqueCliente.cpp
@@ -1,12 +1,54 @@
 #include "autoload.hpp"
 
+#include "./base/json.hpp"
+
+using namespace std;
+
+//https://linuxhint.com/parse-json-data-cpp/
+//https://github.com/nlohmann/json/releases
+
 /**
  * @brief 	Lista o estoque de maneira especial para o cliente
  * 
  */
 void EstoqueCliente::listarEstoque()
 {
-	
+	//percorrer o estoque e listar 
+    std::ifstream file("data/estoque.json");
+    json::value json = json::parse(file);
+
+    const json::array &a = as_array(json);
+
+    int i = 1;
+    // caso o estoque estiver vazio
+    if (a.size() == 0)
+    {
+        return;
+    }
+
+    std::cout<< "+---+----+------+-----------+-----+-------+"<<std::endl;
+    std::cout<< "|   | ID | NOME | DESCRICAO | QTD | PRECO |"<<std::endl;
+
+    for(auto it = a.begin(); it != a.end(); ++it)
+    {
+        /* json::value v;*/
+        nlohmann::json v;
+        std::string idstr = v["id"];
+        std::string nomestr = v["nome"];
+        std::string descstr = v["descricao"];
+        std::string qtdstr = v["qtd"];
+        long int precostr = v["preco"];
+
+        std::cout<<"+-----------------------------------------------------+"<<std::endl;
+        std::cout<< "| "<<i;
+        std::cout<< " | " << idstr << std::endl;
+        std::cout<< " | " << nomestr << std::endl;
+        std::cout<< " | " << descstr << std::endl;
+        std::cout<< " | " << qtdstr << std::endl;
+        std::cout<< " | " << "R$" << setprecision(2) << fixed << precostr << std::endl;
+        i++;
+    }  
+    std::cout<<"+-----------------------------------------------------+"<<std::endl;
 }
 
 /**
@@ -15,6 +57,7 @@ void EstoqueCliente::listarEstoque()
  */
 EstoqueCliente::~EstoqueCliente()
 {
+    //destrutor da classe
 	
 }
 
@@ -24,5 +67,6 @@ EstoqueCliente::~EstoqueCliente()
  */
 EstoqueCliente::EstoqueCliente()
 {
-	
+	//construtor da classe
+
 }
