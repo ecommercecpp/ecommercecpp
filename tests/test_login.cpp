4378b8d4c90b8caaf15abbd9183cbe8b6a56a5dc
Leonardo Oliveira <leooli.teixeira@gmail.com>
2022-07-12 13:34:47 -0300

    feat: :ambulance: adiciona testes de login
    Co-Authored-By: Aquila <97201817+aquila1004@users.noreply.github.com>
    Co-Authored-By: Felippe Veloso <60450622+FelippeVelosoMarinho@users.noreply.github.com>
    Co-Authored-By: CaioHosken <97640125+CaioHosken@users.noreply.github.com>
    Co-Authored-By: EduScf <102927669+EduScf@users.noreply.github.com>

---
+++ tests/test_login.cpp
@@ -0,0 +1,52 @@
+#include "autoload.hpp"
+#include "doctest"
+
+TEST_CASE("Testa o login corretamente")
+{
+	Usuario usuario = Usuario();
+	usuario.setCpf("12312312312");
+	usuario.setSenha("senha");
+	CHECK(usuario.login());
+}
+
+TEST_CASE("Testa o login com usuário que não existe")
+{
+	Usuario usuario = Usuario();
+	usuario.setCpf("32132132132");
+	usuario.setSenha("senha");
+	CHECK_THROWS_AS(usuario.login(), UsuarioNaoEncontradoException);
+}
+
+TEST_CASE("Testa o login com senha invalida")
+{
+	Usuario usuario = Usuario();
+	usuario.setCpf("12312312312");
+	usuario.setSenha("123");
+	CHECK_THROWS_AS(usuario.login(), UsuarioSenhaInvalidaException);
+}
+
+TEST_CASE("Testa o cadastro corretamente")
+{
+	Endereco endereco;
+	endereco.setRua("rua");
+	endereco.setNumero("123");
+	endereco.setBairro("bairro");
+	endereco.setCidade("cidade");
+	endereco.setEstado("estado");
+	endereco.setCep("12312312");
+	Usuario usuario = Usuario("nome", "32132132132", "email", endereco, "senha");
+	CHECK(usuario.cadastro());
+}
+
+TEST_CASE("Testa o cadastro com usuário existente")
+{
+	Endereco endereco;
+	endereco.setRua("rua");
+	endereco.setNumero("123");
+	endereco.setBairro("bairro");
+	endereco.setCidade("cidade");
+	endereco.setEstado("estado");
+	endereco.setCep("12312312");
+	Usuario usuario = Usuario("nome", "12312312312", "email", endereco, "senha");
+	CHECK_THROWS_AS(usuario.cadastro(), UsuarioExistenteException);
+}
